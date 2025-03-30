a2e41de67692f04f1615164d99e58f6e3c53051e
hrzip <63956767+hrzip@users.noreply.github.com>
2025-03-30 20:31:32 +0200

    Zadatak 10 i 11

--- DZ27032025/zadatak10.py
+++ DZ27032025/zadatak10.py
@@ -1,1 +1,9 @@
 # Izračunajte prosječno trajanje filmova u listi koju ste odabrali
+lista_filmova = ["Gospodar Prstenova", "Hachiko", "Pink Panther", "Zvjezdani ratovi", "All Quiet on the Western Front"]
+trajanje_filmova_u_minutama = [178, 93, 93, 121, 147]
+
+prosjecno_trajanje = sum(trajanje_filmova_u_minutama) / len(lista_filmova)
+
+print(f"Popis filmova: {lista_filmova}")
+print()
+print(f"Prosječno trajanje filmova je: {prosjecno_trajanje}")
