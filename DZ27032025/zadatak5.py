b7e1f29e226d8e9ad4c68c1bb9df8e8f92c60e93
hrzip <63956767+hrzip@users.noreply.github.com>
2025-03-30 20:09:43 +0200

    dodani zadaci od 5 do 9

--- DZ27032025/zadatak5.py
+++ DZ27032025/zadatak5.py
@@ -1,1 +1,5 @@
 # Napravite listu s deset omiljenih filmova, sortirajte ih abecedno i ispišite ih na ekran
+
+lista_filmova = ["Gospodar Prstenova", "Hachiko", "Hobit", "Zvjezdani ratovi", "All Quiet on the Western Front", "Harry Potter", "Chernobyl", "Stargate", "Knives out", "Arthur the King"]
+lista_filmova.sort()
+print(lista_filmova)
--- DZ27032025/zadatak7.py
+++ DZ27032025/zadatak7.py
@@ -1,1 +1,17 @@
 # Napravite dvije liste s naslovima filmova i njihovim godinama izlaska. Zatim spojite te dvije liste u jednu listu tako da svaki element bude niz koji sadrži naziv filma i godinu izlaska
+
+naslov_filma = ["Gospodar Prstenova", "Hachiko", "Hobit", "All Quiet on the Western Front"]
+godina_izlaska = [2001, 2009, 2012, 2022]
+
+ispis_stavki = []
+
+for indeks in range(len(naslov_filma)):
+    film = naslov_filma[indeks]
+    godina = godina_izlaska[indeks]
+    ispis_stavki.append([film, godina])
+
+for stavka in ispis_stavki:
+    print(f"{stavka[0]} - {stavka[1]}")
+    
+
+
